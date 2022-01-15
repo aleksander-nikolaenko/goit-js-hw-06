3b306fd2b118d66433adba263af284397c6adbb1
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2022-01-16 01:19:17 +0200

    add solution task-02

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
 
+const user = {};
+const formRef = document.querySelector('.login-form');
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+  user.email = email.value; 
+  user.password = password.value; 
+  
+  console.log(user);
+  event.currentTarget.reset();
+}
+
+formRef.addEventListener("submit", handleSubmit);
