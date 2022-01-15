3b197d05dd65739a7c4b27b51e28b0b99fe5dcb8
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2022-01-15 20:21:47 +0200

    add solution task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsRef = document.querySelector('#ingredients');
+
+const createList = (elements) => {
+  const List = elements.map((element) => {
+    const listItem = document.createElement('li');
+    listItem.classList.add('item');
+    listItem.textContent = element;
+    return listItem;
+  });
+  return List;
+}
+
+const ingredientsList = createList(ingredients)
+ingredientsRef.append(...ingredientsList);
