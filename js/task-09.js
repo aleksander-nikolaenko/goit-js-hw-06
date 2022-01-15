8ebe74c85b4e3ecb6fc6115d4b8b75787728ead8
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2022-01-16 01:43:08 +0200

    add solution task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const bodyRef = document.querySelector("body");
+const colorTextField = document.querySelector('span.color');
+const changeColorBtn = document.querySelector("button.change-color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColor = () => {
+  const randomColor = getRandomHexColor();
+  colorTextField.textContent = randomColor;
+  bodyRef.style.backgroundColor = randomColor;
+}
+
+changeColorBtn.addEventListener("click", changeColor);
